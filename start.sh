324b56f4e956983efda1f40505071772756c2070
Ken <kenin.qiu@gmail.com>
2023-11-20 13:39:56 -0500

    f

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+mv ${PWD}/organizations/peerOrganizations/aab.dnb/users/Admin@aab.dnb/msp/keystore/* ${PWD}/organizations/peerOrganizations/aab.dnb/users/Admin@aab.dnb/msp/keystore/key.pem
 export EXPLORER_CONFIG_FILE_PATH=./config.json
 export EXPLORER_PROFILE_DIR_PATH=./connection-profile
 export FABRIC_CRYPTO_PATH=./organizations
